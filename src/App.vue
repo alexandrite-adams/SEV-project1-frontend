f5f1ad5ed0faa65e0385e7ae9c55fd55aa2f2cdf
Alexandria Adams <alexm.adams@yahoo.com>
2023-02-15 13:51:20 -0600

    Resetting up

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-layout>
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title >OC Music Department</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 500px;">
+        <v-card-title variant="tonal">
+          Your upcoming events:
+        </v-card-title>
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+const myCustomTheme = {
+  dark: false,
+  colors: {
+    primary: '#800020',
+  }
+}
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme,
+    }
+  }
+})
