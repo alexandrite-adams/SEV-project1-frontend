94667113cbfe54285b1afc17054f191536f0e3d4
Scott Cunningham <scott.cunningham@eagles.oc.edu>
2023-02-26 15:36:50 -0600

    Merging new dev

---
+++ src/components/AdminCreateEvent.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid class="fill-height bg-primary">
+    <v-list class="fill-height bg-primary" :items="items"></v-list>
+  </v-container>
+</template>
+
+<select
+  name="LeaveType"
+  @change="onChange($event)"
+  class="form-control"
+  v-model="key"
+>
+   <option value="1"> Jury </option>
+   <option value="2"> Review </option>
+
+</select>
+
+<script>
+var vm = new Vue({
+    data: {
+        key: ""
+    },
+    methods: {
+        onChange(event) {
+            console.log(event.target.value)
+        }
+    }
+</script>
