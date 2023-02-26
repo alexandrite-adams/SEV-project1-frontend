592aee258c7be6f864f00fa0164540959e58df2f
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-02-26 15:24:32 -0600

    added services

--- src/components/faculty/CritiqueView.vue
+++ src/components/faculty/CritiqueView.vue
@@ -1,34 +1,25 @@
 <template>
-  <!-- <v-radio-group inline label="Search by:">
-        <v-radio @change="searchBy('event')" label="Event Date" value="1"></v-radio>
-        <v-radio @change="searchBy('student')" label="Student Name" value="2"></v-radio>
-    </v-radio-group> -->
   <v-select
+    v-model="searchByIndex"
     label="Search by"
     :items="searchByList"
-    @update:modelValue="changeIndex()"
   ></v-select>
 
   <v-divider></v-divider>
 
-  <v-container v-if="searchByIndex == 1">test 1</v-container>
-  <v-container v-else-if="searchByIndex == 2">test 2</v-container>
+  <v-container v-if="searchByIndex == 'Event Date'">test 1</v-container>
+  <v-container v-else-if="searchByIndex == 'Student Name'">test 2</v-container>
 </template>
 <script>
 export default {
+  name: "facultyCritiqueView",
   data: () => ({
     searchByIndex: null,
     searchByList: ["Event Date", "Student Name"],
   }),
   methods: {
-    changeIndex() {
-      this.searchByIndex = 2;
-      console.log(this.searchByIndex);
-    },
-    searchBy(value) {
-      searchByIndex = value;
-      console.log("search by-" + value);
-    },
+    async retrieveAllSemesters() {},
+    async retrieveEventsBySemester(semester) {},
   },
 };
 </script>
